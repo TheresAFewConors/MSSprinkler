c7dea31668c5557f65b3d60d89027a50d9f6d6f0
TheresAFewConors <TheresAFewConors@Protonmail.com>
2024-12-18 14:37:05 +0000

    correct release value

--- PowerShellModules/PowerShellModule.psm1
+++ PowerShellModules/PowerShellModule.psm1
@@ -3,4 +3,4 @@
 . $PSScriptRoot\functions\GetTenantId.ps1
 . $PSScriptRoot\functions\GetUserToken.ps1
 #. $PSScriptRoot\functions\InvokeValidateUsersExternal.ps1
-$moduleVersion = '0.2'
+$moduleVersion = '0.2.5'
